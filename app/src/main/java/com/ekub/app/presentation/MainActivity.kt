67c22430118f8448886a1e02a9b8458d19f62c5f
abrhamobere789-png <abrhamobere789@gmail.com>
2026-09-08 23:02:57 +0300

    Add MainActivity with Compose support

---
+++ app/src/main/java/com/ekub/app/presentation/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.ekub.app.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.ekub.app.presentation.ui.theme.AndroidEkubAppTheme
+import com.ekub.app.presentation.navigation.RootNavigation
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidEkubAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    RootNavigation()
+                }
+            }
+        }
+    }
+}
