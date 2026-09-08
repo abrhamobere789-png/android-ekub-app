4d3242226d13878c465f032347e2960b2f920382
abrhamobere789-png <abrhamobere789@gmail.com>
2026-09-08 23:00:27 +0300

    Add root build.gradle.kts with plugin declarations

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("com.android.library") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.9.0" apply false
+    id("com.google.dagger.hilt.android") version "2.46" apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
+}
+
+task("clean") {
+    delete(rootProject.buildDir)
+}
